dcb71f395a40b3b834072798d7869fea8ca2df45
Guilherme Schulz <gui.schulz@gmail.com>
2023-07-21 20:17:13 -0300

    finalizado rota create

--- magpy/api/serializers.py
+++ magpy/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-
+from rest_framework.exceptions import ValidationError
 from .models import PackageRelease, Project
+import requests
 
 
 class PackageSerializer(serializers.ModelSerializer):
@@ -18,8 +19,20 @@ class ProjectSerializer(serializers.ModelSerializer):
     packages = PackageSerializer(many=True)
 
     def create(self, validated_data):
-        # TODO
-        # - Processar os pacotes recebidos
-        # - Persistir informações no banco
+        project = Project(name=validated_data['name'])
+        project.save()
         packages = validated_data['packages']
-        return Project(name=validated_data['name'])
+        for package in packages:
+            r = requests.get(f'https://pypi.org/pypi/{package["name"]}/json')
+            if r.status_code == 200:
+                if 'version' not in package:
+                    package['version'] = r.json()['info']['version']
+            else:
+                project.delete()
+                raise ValidationError(
+                    {"error": "One or more packages doesn't exist"})
+            package['project'] = project
+            package = PackageRelease(**package)
+            print(package)
+            package.save()
+        return project
